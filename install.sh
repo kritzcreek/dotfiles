5a872fce43ab057ffbb0c07f71cbbeb98e7850db
Christoph Hegemann <christoph.hegemann1337@gmail.com>
2015-10-11 13:38:28 +0200

    add more things

--- install.sh
+++ install.sh
@@ -4,6 +4,10 @@ ln -fs ~/dotfiles/.zshrc ~/.zshrc
 ln -fs ~/dotfiles/.aliases ~/.aliases
 ln -fs ~/dotfiles/.exports ~/.exports
 ln -fs ~/dotfiles/.functions ~/.functions
+ln -fs ~/dotfiles/.xinitrc ~/.xinitrc
+ln -fs ~/dotfiles/.xprofile ~/.xprofile
+ln -fs ~/dotfiles/.Xdefaults ~/.Xdefaults
+ln -fs ~/dotfiles/.gitconfig ~/.gitconfig
 
 # Installing Arch
 echo "Now execute install_arch.sh"

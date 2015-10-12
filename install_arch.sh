3bdb1d4a779f184e5d2f9a080dd43cbce2bf254c
Christoph Hegemann <christoph.hegemann1337@gmail.com>
2015-10-12 17:08:56 +0200

    some more changes

--- install_arch.sh
+++ install_arch.sh
@@ -6,7 +6,10 @@ pwd=`cd "$dir" > /dev/null 2>&1 && pwd`
 sudo pacman -Syu
 sudo pacman -S --noconfirm yaourt base-devel
 sudo pacman -S --noconfirm emacs xmonad xmonad-contrib zsh git curl feh dmenu
-sudo pacman -S --noconfirm openssh the_silver_searcher
+sudo pacman -S --noconfirm openssh the_silver_searcher rxvt-unicode unzip
+
+# Install Spacemacs
+git clone https://github.com/syl20bnr/spacemacs -b develop ~/.emacs
 
 chsh -s /usr/bin/zsh
 # sudo pacman -S texlive-most texlive-langcyrillic

e1ff3e491799413ca6a43e79ef04ad70418c4e75
Christoph Hegemann <christoph.hegemann1337@gmail.com>
2015-10-12 19:07:35 +0200

    transparency

--- install_arch.sh
+++ install_arch.sh
@@ -6,7 +6,7 @@ pwd=`cd "$dir" > /dev/null 2>&1 && pwd`
 sudo pacman -Syu
 sudo pacman -S --noconfirm yaourt base-devel
 sudo pacman -S --noconfirm emacs xmonad xmonad-contrib zsh git curl feh dmenu
-sudo pacman -S --noconfirm openssh the_silver_searcher rxvt-unicode unzip
+sudo pacman -S --noconfirm openssh the_silver_searcher rxvt-unicode unzip xcompmgr
 
 # Install Spacemacs
 git clone https://github.com/syl20bnr/spacemacs -b develop ~/.emacs

7ef71bde7cfe6a6bf6647c6af2cb10213ab04ab2
Christoph Hegemann <christoph.hegemann1337@gmail.com>
2016-02-06 13:35:37 +0100

    move path into zshenv.
    turn syntax highlighting for zsh back on

---
+++ .zshenv
@@ -0,0 +1,3 @@
+
+typeset -U path
+path=(~/.local/bin $path)
--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 # Symlinking
 ln -fvs ~/dotfiles/.spacemacs.d ~/.spacemacs.d
 ln -fvs ~/dotfiles/.zshrc ~/.zshrc
+ln -fvs ~/dotfiles/.zshenv ~/.zshenv
 ln -fvs ~/dotfiles/.aliases ~/.aliases
 ln -fvs ~/dotfiles/.exports ~/.exports
 ln -fvs ~/dotfiles/.functions ~/.functions

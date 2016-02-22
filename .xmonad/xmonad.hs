9883023b9e6b6f7339dcf5789d4de3f5bccd2b42
Christoph Hegemann <christoph.hegemann1337@gmail.com>
2016-02-22 21:31:30 +0100

    fix warnings in xmonad.hs

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,84 +1,28 @@
---
--- xmonad example config file for xmonad-0.9
---
--- A template showing all available configuration hooks,
--- and how to override the defaults in your own xmonad.hs conf file.
---
--- Normally, you'd only override those defaults you care about.
---
--- NOTE: Those updating from earlier xmonad versions, who use
--- EwmhDesktops, safeSpawn, WindowGo, or the simple-status-bar
--- setup functions (dzen, xmobar) probably need to change
--- xmonad.hs, please see the notes below, or the following
--- link for more details:
---
--- http://www.haskell.org/haskellwiki/Xmonad/Notable_changes_since_0.8
---
+import           Data.Monoid              (All)
+import           System.Exit
+import           System.IO                (Handle)
+import           XMonad
+import           XMonad.Hooks.DynamicLog
+import           XMonad.Hooks.ManageDocks
+import           XMonad.Hooks.SetWMName
+import           XMonad.Layout.Spacing
+import           XMonad.Util.Run
 
-import XMonad
-import Data.Monoid
-import System.Exit
-import XMonad.Hooks.SetWMName
-import XMonad.Hooks.ManageDocks
-import XMonad.Hooks.DynamicLog
-import XMonad.Layout.Spacing
-import XMonad.Util.Run
+import qualified Data.Map                 as M
+import qualified XMonad.StackSet          as W
 
-import qualified XMonad.StackSet as W
-import qualified Data.Map        as M
+myTerminal :: String
+myTerminal = "urxvt"
 
--- The preferred terminal program, which is used in a binding below and by
--- certain contrib modules.
---
-myTerminal      = "urxvt"
-
--- Whether focus follows the mouse pointer.
 myFocusFollowsMouse :: Bool
 myFocusFollowsMouse = True
 
--- Width of the window border in pixels.
---
+myBorderWidth :: Dimension
 myBorderWidth   = 2
-
--- modMask lets you specify which modkey you want to use. The default
--- is mod1Mask ("left alt").  You may also consider using mod3Mask
--- ("right alt"), which does not conflict with emacs keybindings. The
--- "windows key" is usually mod4Mask.
---
+myModMask :: KeyMask
 myModMask       = mod4Mask
 
--- NOTE: from 0.9.1 on numlock mask is set automatically. The numlockMask
--- setting should be removed from configs.
---
--- You can safely remove this even on earlier xmonad versions unless you
--- need to set it to something other than the default mod2Mask, (e.g. OSX).
---
--- The mask for the numlock key. Numlock status is "masked" from the
--- current modifier status, so the keybindings will work with numlock on or
--- off. You may need to change this on some systems.
---
--- You can find the numlock modifier by running "xmodmap" and looking for a
--- modifier with Num_Lock bound to it:
---
--- > $ xmodmap | grep Num
--- > mod2        Num_Lock (0x4d)
---
--- Set numlockMask = 0 if you don't have a numlock key, or want to treat
--- numlock status separately.
---
--- myNumlockMask   = mod2Mask -- deprecated in xmonad-0.9.1
-------------------------------------------------------------
-
-
--- The default number of workspaces (virtual screens) and their names.
--- By default we use numeric strings, but any string may be used as a
--- workspace name. The number of workspaces is determined by the length
--- of this list.
---
--- A tagging example:
---
--- > workspaces = ["web", "irc", "code" ] ++ map show [4..9]
---
+myWorkspaces :: [String]
 myWorkspaces = [devel, code, web, music, media, mail, misc]
                where music = "   ^i(/home/creek/.xmonad/icons/Music.xbm)"
                      web   = "   ^i(/home/creek/.xmonad/icons/www.xbm)"
@@ -89,13 +33,16 @@ myWorkspaces = [devel, code, web, music, media, mail, misc]
                      misc  = "   ^i(/home/creek/.xmonad/icons/pacman.xbm)"
 -- Border colors for unfocused and focused windows, respectively.
 --
+myNormalBorderColor :: String
 myNormalBorderColor  = "#dddddd"
+myFocusedBorderColor :: String
 myFocusedBorderColor = "#00ff00"
 
 ------------------------------------------------------------------------
 -- Key bindings. Add, modify or remove key bindings here.
 --
-myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
+myKeys :: XConfig Layout -> M.Map (KeyMask, KeySym) (X ())
+myKeys conf@XConfig{XMonad.modMask = modm} = M.fromList $
 
     -- launch a terminal
     [ ((modm .|. shiftMask, xK_Return), spawn $ XMonad.terminal conf)
@@ -158,9 +105,6 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
     , ((modm              , xK_period), sendMessage (IncMasterN (-1)))
 
     -- Toggle the status bar gap
-    -- Use this binding with avoidStruts from Hooks.ManageDocks.
-    -- See also the statusBar function from Hooks.DynamicLog.
-    --
     , ((modm              , xK_b     ), sendMessage ToggleStruts)
 
     -- Quit xmonad
@@ -193,114 +137,46 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
 ------------------------------------------------------------------------
 -- Mouse bindings: default actions bound to mouse events
 --
-myMouseBindings (XConfig {XMonad.modMask = modm}) = M.fromList $
-
+myMouseBindings
+  :: XConfig t -> M.Map (KeyMask, Button) (Window -> X ())
+myMouseBindings XConfig{XMonad.modMask = modm} =
+  M.fromList
     -- mod-button1, Set the window to floating mode and move by dragging
-    [ ((modm, button1), (\w -> focus w >> mouseMoveWindow w
-                                       >> windows W.shiftMaster))
-
+    [ ((modm, button1), \w -> focus w >> mouseMoveWindow w
+                                      >> windows W.shiftMaster)
     -- mod-button2, Raise the window to the top of the stack
-    , ((modm, button2), (\w -> focus w >> windows W.shiftMaster))
-
+    , ((modm, button2), \w -> focus w >> windows W.shiftMaster)
     -- mod-button3, Set the window to floating mode and resize by dragging
-    , ((modm, button3), (\w -> focus w >> mouseResizeWindow w
-                                       >> windows W.shiftMaster))
-
-    -- you may also bind events to the mouse scroll wheel (button4 and button5)
+    , ((modm, button3), \w -> focus w >> mouseResizeWindow w
+                                      >> windows W.shiftMaster)
     ]
 
-------------------------------------------------------------------------
--- Layouts:
-
--- You can specify and transform your layouts by modifying these values.
--- If you change layout bindings be sure to use 'mod-shift-space' after
--- restarting (with 'mod-q') to reset your layout state to the new
--- defaults, as xmonad preserves your old layout settings by default.
---
--- * NOTE: XMonad.Hooks.EwmhDesktops users must remove the obsolete
--- ewmhDesktopsLayout modifier from layoutHook. It no longer exists.
--- Instead use the 'ewmh' function from that module to modify your
--- defaultConfig as a whole. (See also logHook, handleEventHook, and
--- startupHook ewmh notes.)
---
--- The available layouts.  Note that each layout is separated by |||,
--- which denotes layout choice.
---
+myLayout :: Choose Tall (Choose (Mirror Tall) Full) a
 myLayout = tiled ||| Mirror tiled ||| Full
   where
-    -- default tiling algorithm partitions the screen into two panes
     tiled   = Tall nmaster delta ratio
-
-    -- The default number of windows in the master pane
     nmaster = 1
-
-    -- Default proportion of screen occupied by master pane
     ratio   = 3/5
-
-    -- Percent of screen to increment by when resizing panes
     delta   = 3/100
 
-------------------------------------------------------------------------
--- Window rules:
-
--- Execute arbitrary actions and WindowSet manipulations when managing
--- a new window. You can use this to, for example, always float a
--- particular program, or have a client always appear on a particular
--- workspace.
---
--- To find the property name associated with a program, use
--- > xprop | grep WM_CLASS
--- and click on the client you're interested in.
---
--- To match on the WM_NAME, you can use 'title' in the same way that
--- 'className' and 'resource' are used below.
---
+myManageHook :: ManageHook
 myManageHook = composeAll []
 
-------------------------------------------------------------------------
--- Event handling
-
--- Defines a custom handler function for X Events. The function should
--- return (All True) if the default handler is to be run afterwards. To
--- combine event hooks use mappend or mconcat from Data.Monoid.
---
--- * NOTE: EwmhDesktops users should use the 'ewmh' function from
--- XMonad.Hooks.EwmhDesktops to modify their defaultConfig as a whole.
--- It will add EWMH event handling to your custom event hooks by
--- combining them with ewmhDesktopsEventHook.
---
+myEventHook :: Event -> X All
 myEventHook = mempty
 
-------------------------------------------------------------------------
--- Status bars and logging
-
--- Perform an arbitrary action on each internal state change or X event.
--- See the 'XMonad.Hooks.DynamicLog' extension for examples.
---
---
--- * NOTE: EwmhDesktops users should use the 'ewmh' function from
--- XMonad.Hooks.EwmhDesktops to modify their defaultConfig as a whole.
--- It will add EWMH logHook actions to your custom log hook by
--- combining it with ewmhDesktopsLogHook.
---
--- myLogHook h =
---     let pp = defaultPP { ppTitle = xmobarColor "teal" "" . shorten 80
---               , ppCurrent = xmobarColor "yellow" "" . wrap "[" "]"
---               , ppVisible = wrap "(" ")"
---               , ppWsSep = "|"
---               , ppLayout = const ""
---               }
---     in dynamicLogWithPP pp { ppOutput = hPutStrLn h }
-
-myLogHook dzproc = dynamicLogWithPP $ dzenPP {
+myLogHook :: Handle -> X ()
+myLogHook dzproc =
+  dynamicLogWithPP $ dzenPP {
       ppOutput = hPutStrLn dzproc
     , ppTitle =  pad  . shorten 50
-    , ppLayout = dzenColor color4 background . (\x -> case x of
-                                             "Spacing 10 Tall" -> "    ^i(/home/creek/.xmonad/icons/tiling.xbm)"
-                                             "Spacing 10 Mirror Tall" -> "    ^i(/home/creek/.xmonad/icons/mirrortall.xbm)"
-                                             "Spacing 10 Full" -> "    ^i(/home/creek/.xmonad/icons/mirrortall.xbm)"
-                                             _ -> "  New Layout "
-                                             )
+    , ppLayout = dzenColor color4 background .
+      (\x -> case x of
+        "Spacing 10 Tall" -> "    ^i(/home/creek/.xmonad/icons/tiling.xbm)"
+        "Spacing 10 Mirror Tall" -> "    ^i(/home/creek/.xmonad/icons/mirrortall.xbm)"
+        "Spacing 10 Full" -> "    ^i(/home/creek/.xmonad/icons/mirrortall.xbm)"
+        _ -> "  New Layout "
+      )
 
     , ppCurrent = dzenColor foreground background -- foreground "#FF6000"
     , ppVisible = dzenColor color4 background
@@ -308,43 +184,31 @@ myLogHook dzproc = dynamicLogWithPP $ dzenPP {
     , ppHiddenNoWindows = dzenColor color8 background
     , ppOrder = \(ws:l:t:_) -> [ws,l,t]
     }
-------------------------------------------------------------------------
--- Startup hook
 
--- Perform an arbitrary action each time xmonad starts or is restarted
--- with mod-q.  Used by, e.g., XMonad.Layout.PerWorkspace to initialize
--- per-workspace layout choices.
---
--- By default, do nothing.
---
--- * NOTE: EwmhDesktops users should use the 'ewmh' function from
--- XMonad.Hooks.EwmhDesktops to modify their defaultConfig as a whole.
--- It will add initialization of EWMH support to your custom startup
--- hook by combining it with ewmhDesktopsStartup.
---
+myStartupHook :: X ()
 myStartupHook = setWMName "LG3D"
 
-------------------------------------------------------------------------
--- Now run xmonad with all the defaults we set up.
+bar1Width = "800"
+bar2Width = "566"
 
--- Run xmonad with the settings you specify. No need to modify this.
---
-
-myStatusBar, myConkyBar, myFont :: String
+myStatusBar, myConkyBar, bar1Width, bar2Width :: String
 myStatusBar =
-  "dzen2 -x 0 -w '1000' -h '28' -ta l -xs 1 -fg '" ++ foreground ++ "' -bg '" ++ background ++ "' -fn '" ++ myFont ++ "'"
+  "dzen2 -x 0 -w '"
+  ++ bar1Width ++ "' -h '28' -ta l -xs 1 -fg '"
+  ++ foreground ++ "' -bg '" ++ background ++ "' -fn '"
+  ++ myFont ++ "'"
 myConkyBar =
-  "conky -c ~/.xmonad/conky_dzen | dzen2 -ta r -x '1000' -w '920' -h '28' -p $OPTS -xs 1 -fg '" ++ foreground ++ "' -bg '" ++ background ++ "' -fn '" ++ myFont ++ "'"
+  "conky -c ~/.xmonad/conky_dzen | dzen2 -ta r -x '"
+  ++ bar1Width ++"' -w '"
+  ++ bar2Width ++ "' -h '28' -p $OPTS -xs 1 -fg '"
+  ++ foreground ++ "' -bg '" ++ background
+  ++ "' -fn '" ++ myFont ++ "'"
 
-
-
-
+main :: IO ()
 main = do
-  dzproc <- spawnPipe $ myStatusBar
-  dzstatus <- spawnPipe $ myConkyBar
-  -- h <- spawnPipe "xmobar"
-  xmonad $ defaultConfig {
-      -- simple stuff
+  dzproc <- spawnPipe myStatusBar
+  _ <- spawnPipe myConkyBar
+  xmonad $ def {
         terminal           = myTerminal,
         focusFollowsMouse  = myFocusFollowsMouse,
         borderWidth        = myBorderWidth,
@@ -352,36 +216,21 @@ main = do
         workspaces         = myWorkspaces,
         normalBorderColor  = myNormalBorderColor,
         focusedBorderColor = myFocusedBorderColor,
-
-      -- key bindings
         keys               = myKeys,
         mouseBindings      = myMouseBindings,
-
-      -- hooks, layouts
-        layoutHook         = smartSpacing 40 $ avoidStruts myLayout,
+        layoutHook         = smartSpacing 5 $ avoidStruts myLayout,
         manageHook         = myManageHook <+> manageDocks,
         handleEventHook    = myEventHook <+> docksEventHook,
         logHook            = myLogHook dzproc,
         startupHook        = myStartupHook
     }
 
+color8, color4, myFont, background, foreground :: String
+
 myFont = "-*-Source Code Pro-*-*-*-*-15-*-*-*-*-*-*-*"
 
-background= "#232323" -- "#0E0E0E"
-foreground= "#CBCBCB"
-color0= "#454545"
-color8= "#676767"
-color1=  "#CC4747"
-color9=  "#BF5858"
-color2=  "#A0CF5D"
-color10= "#B8D68C"
-color3=  "#FF9B52"
-color11= "#FFB680"
-color4=  "#307D92" -- "#508934" -- "#AB2010" -- "#99492F"
-color12= "#99C7BF"
-color5=  "#A966CC"
-color13= "#BD9FCC"
-color6=  "#6CAB79"
-color14= "#95BA9C"
-color7=  "#d3d3d3"
-color15= "#fefefe"
+background = "#232323"
+foreground = "#CBCBCB"
+color8 = "#676767"
+color4 = "#307D92"
+
